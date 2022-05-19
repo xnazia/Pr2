d0f235720b2fe70d3403b718a8af0fc8f115c5fe
Nazia <wi21b122@technikum-wien.at>
2022-05-19 15:57:09 +0200

    add sum feature

---
+++ src/main/java/com/example/pr2/CalculatorController.java
@@ -0,0 +1,18 @@
+package com.example.pr2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+@RequestMapping("/sum")
+    public int sum(
+        @RequestParam int a,
+        @RequestParam int b)
+{
+        return a+b;
+    }
+
+
+
+}
