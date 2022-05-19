473946c7080bdf1f6bb37bcb93122d0f8bdc9b90
Nazia <wi21b122@technikum-wien.at>
2022-05-19 15:34:25 +0200

    commit

---
+++ src/main/java/com/example/pr2/Pr2Application.java
@@ -0,0 +1,13 @@
+package com.example.pr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pr2/Pr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
