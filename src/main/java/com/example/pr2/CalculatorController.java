49ed74432741a8f8718aff33a22cf31ba12113ba
Nazia <wi21b122@technikum-wien.at>
2022-06-15 11:56:54 +0200

    add multiply feature

--- src/main/java/com/example/pr2/CalculatorController.java
+++ src/main/java/com/example/pr2/CalculatorController.java
@@ -5,23 +5,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-@RequestMapping("/sum")
+
+    Calculator calculator = new Calculator();
+    @RequestMapping("/sum")
     public int sum(
-        @RequestParam int a,
-        @RequestParam int b)
-{
-        return a+b;
+            @RequestParam int a,
+            @RequestParam int b) {
+        return a + b;
     }
 
-
+//multiply
     @RequestMapping("/multiply")
     public int multiply(
             @RequestParam int a,
-            @RequestParam int b)
-    {
-        return a*b;
+            @RequestParam int b) {
+        return a * b;
     }
 
+//absolute
+    @RequestMapping("/absolute")
+    public int absolute(
+            @RequestParam int a
+            ) {
+        if (a < 0){
+            return a*-1;
+        }
+        else {
+            return a;
+        }
+    }
+    //division
+
+    @RequestMapping("/division")
+    public double division(
+            @RequestParam int a,
+            @RequestParam int b) {
+        return a / b;
+    }
+
+    //minus
+
+    @RequestMapping("/minus")
+    public double minus(
+            @RequestParam int a,
+            @RequestParam int b) {
+        return a - b;
+    }
+
+
+    //quadrieren
+    @RequestMapping("/square")
+    public int square(
+            @RequestParam int a) {
+        return a*a;
+    }
 
 
 
